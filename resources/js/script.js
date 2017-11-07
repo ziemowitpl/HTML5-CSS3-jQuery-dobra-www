8e572701ad21ea25d6c61dbd7030ad25b13712bd
ziemowitpl <roninrallegro@gmail.com>
2017-11-07 14:57:20 +0100

    Robimy Scroll on buttons które przeżuci nas do sekcji poniżej w 1 sekundę. Nadajemy wpierw pierwszemu przyciskowi Im hungry klasę js--scroll-to-plans a drugiemu js--scroll-to-start. Teraz nadajemy sekcją klasy js--section-plans oraz js-section-features. Następnie piszemy kod jQuery Dla .js-scroll-to-plans na kliknięcie wykonaj funkcję w html oraz body animacji, przewiń na sekcję stając na górze .js--section-plans, offset (nie wiem po co) góra i zrób to w 1000ms czyli w jedną sekundę. Podobnie dla drugiego przycisku...; Navigation scroll tu wklejamy kod z css-tricks smooth-scrolling a w dokumencie html dla przycisku Food delivery w linku w href= - #features - a w .section-features tworzymy id z wartością - features - i cała filozofia.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+    // For the sticky navigation
     $('.js--section-features').waypoint(function(direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -10,11 +11,27 @@ $(document).ready(function() {
         offset: '120px'
     });
 
+    // Scroll on buttons
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
 
-//     var waypoints = $('#handler-first').waypoint(function(direction) {
-//   notify(this.element.id + ' hit 25% from top of window')
-// }, {
-//   offset: '25%'
-// })
-
+    // Navigation scroll
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+    }
+  });
+});
 });
